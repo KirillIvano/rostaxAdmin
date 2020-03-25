c70cb53b5f933fd611b93e5cf079eb31f788cfc3
KirillIvano <ivannadez@yandex.ru>
2020-03-25 19:49:34 +0300

    added names typings

--- src/entities/message/names.js
+++ src/entities/message/names.js
@@ -1,4 +1,4 @@
 // @flow
 
-export const PUSH_MESSAGE = 'PUSH_MESSAGE';
-export const POP_MESSAGE = 'POP_MESSAGE';
+export const PUSH_MESSAGE: 'PUSH_MESSAGE' = 'PUSH_MESSAGE';
+export const POP_MESSAGE: 'POP_MESSAGE' = 'POP_MESSAGE';
